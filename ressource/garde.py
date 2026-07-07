3d4ede698d2c553f68fb86822b5004799ce9b53d
Ugo-Pas <ugo.pascallon@epitech.eu>
2026-07-07 15:15:28 +0200

    feat(bot): update guard list to use async functions and improve formatting

--- ressource/garde.py
+++ ressource/garde.py
@@ -1,20 +1,24 @@
+import discord
+
+from ressource.AER_team import ALESSANDRO, CZ, LUCAS, MAYA, UGO, _format_discord_user
 from ressource.time import get_time
 
 WEEK = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
-TEAM_AER = ["Ugo PASCALLON", "Alessandro PARIS", "Lucas EECKHOUTTE", "Maya DENTAL", "Celenzo PEUCH", "Close", "Close"] 
+TEAM_AER = [UGO, ALESSANDRO, LUCAS, MAYA, CZ, "Close", "Close"] 
 
 SEPARATOR = "\n---------------------------\n"
 
-def guard_list():
+
+async def guard_list(client: discord.Bot):
     i = 0 
     calandar = """"""
     while i <= 6: 
-        calandar = calandar + calandar_guard(TEAM_AER[i], WEEK[i]) + SEPARATOR
+        calandar = calandar + await calandar_guard(client, TEAM_AER[i], WEEK[i]) + SEPARATOR
         i += 1 
     return calandar
 
 
-def calandar_guard(Name:str, Day:str) -> str:
+async def calandar_guard(client: discord.Bot, Name, Day:str) -> str:
     if Name == "Close" : 
         calandar = f"""
             {Day} : {get_time()[1]}
@@ -24,13 +28,14 @@ def calandar_guard(Name:str, Day:str) -> str:
         """
         return calandar
     else :
+        discord_name = await _format_discord_user(client, Name[1])
         calandar =f"""
         {Day} : {get_time()[1]}
 
-    AER : {Name}
+    AER : {Name[0]}
         
         Contacter :
-            Email -> 
-            Discord ->
+            Email -> {Name[2]}
+            Discord -> {discord_name}
         """
         return calandar
